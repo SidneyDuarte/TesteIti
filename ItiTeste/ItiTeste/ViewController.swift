27571e2153b401ac1db855570ac75f09ce8bd067
Sidney Silva <sidney_silva13@outlook.com>
2019-08-24 20:28:13 -0300

    initial commit

---
+++ ItiTeste/ItiTeste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItiTeste
+//
+//  Created by Sidney Silva on 24/08/19.
+//  Copyright © 2019 Sakura Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
